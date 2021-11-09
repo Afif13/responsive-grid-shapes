a246cf81604fcf6bd50621b207bffde0a615b2d2
Temani Afif <temani.afif@gmail.com>
2021-11-09 21:55:12 +0100

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,16 @@
+let inputs = document.querySelectorAll('input[type=range]')
+let elem = document.querySelector('.main')
+
+
+inputs.forEach(input => {
+   input.addEventListener('change', function(e) {
+      var p = e.target.getAttribute('name');
+      if(p=="s" || p=="mv") {
+        elem.style.setProperty("--"+p, this.value+"px");
+	    	e.target.previousElementSibling.innerHTML = this.value+"px";
+      } else { 
+        elem.style.setProperty("--"+p, this.value);
+  		  e.target.previousElementSibling.innerHTML = this.value;
+      }
+    });
+});
